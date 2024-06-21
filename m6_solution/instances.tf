8e69cdd141eceb2f12ba7688dea283ef2ab64fb4
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-06-21 14:08:41 -0400

    modify m6 instances and s3

--- m6_solution/instances.tf
+++ m6_solution/instances.tf
@@ -14,8 +14,8 @@ data "aws_ssm_parameter" "ami" {
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.subnet1.id
-  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  subnet_id              = aws_subnet.public_subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
   depends_on             = [aws_iam_role_policy.allow_s3_all]
 
@@ -37,8 +37,8 @@ EOF
 resource "aws_instance" "nginx2" {
   ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.subnet2.id
-  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  subnet_id              = aws_subnet.public_subnet2.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
   depends_on             = [aws_iam_role_policy.allow_s3_all]
 
